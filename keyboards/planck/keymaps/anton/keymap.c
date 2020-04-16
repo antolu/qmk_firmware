eb1b4c445bcfb03452938a4dbf85e4ed2d67b108
Anton Lu <anton96lu@gmail.com>
2020-04-16 16:49:21 +0200

    add plover

--- keyboards/planck/keymaps/anton/keymap.c
+++ keyboards/planck/keymaps/anton/keymap.c
@@ -23,7 +23,10 @@
 #ifdef AUDIO_ENABLE
 float se_song[][2]     = SONG(QWERTY_SOUND);
 float us_song[][2]     = SONG(COLEMAK_SOUND);
+float plover_song[][2] = SONG(PLOVER_SOUND); 
+float plover_gb_song[][2] = SONG(PLOVER_GOODBYE_SOUND);
 float numpad_song[][2] = SONG(NUM_LOCK_ON_SOUND);
+float rickroll[][2]    = SONG(RICK_ROLL);
 #endif
 
 extern keymap_config_t keymap_config;
@@ -36,6 +39,7 @@ enum planck_layers {
   _LOWER_US,
   _RAISE_US,
   _NUMPAD,
+  _PLOVER,
   _ADJUST
 };
 
@@ -46,9 +50,12 @@ enum planck_keycodes {
   RAISE_SE,
   LOWER_US,
   RAISE_US,
+  PLOVER,
+  EXT_PLV,
   R_ARROW, 
   KC_STD,
   NUMPAD, 
+  RICKRL,
   BACKLIT
 };
 
@@ -177,14 +184,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_P7,   KC_P8,   KC_P9, KC_MPLY,  _______,  _______,  _______, _______, _______, _______, _______, \
   KC_ESC,  KC_P4,   KC_P5,   KC_P6, KC_PMNS,  _______,  _______,  _______, _______, _______, _______, _______, \
   KC_BSPC, KC_P1,   KC_P2,   KC_P3, KC_PPLS,  _______,  _______,  _______, _______, _______, _______, _______, \
-  KC_PENT, KC_P0, KC_PDOT, KC_PSLS, LOWER_US, _______,  _______, RAISE_US, _______, _______, _______, _______  \
+  KC_PENT, KC_P0, KC_PDOT, KC_PSLS, LOWER_SE, _______,  _______, RAISE_SE, _______, _______, _______, _______  \
+),
+
+/* Plover layer (http://opensteno.org)
+ * ,-----------------------------------------------------------------------------------.
+ * |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |   S  |   T  |   P  |   H  |   *  |   *  |   F  |   P  |   L  |   T  |   D  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |   S  |   K  |   W  |   R  |   *  |   *  |   R  |   B  |   G  |   S  |   Z  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Exit |      |      |   A  |   O  |             |   E  |   U  |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_PLOVER] = LAYOUT_planck_grid(
+    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1   ,
+    XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+    XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|AudOff|AGnorm|AGswap| sv_SE| en_US| NMPAD|      |      |
+ * |      | RICKR|      |Aud on|AudOff|AGnorm|AGswap| sv_SE| en_US| NMPAD| PLOVR|      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      | NMLCK|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -193,7 +218,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid( \
   _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON,   TERM_OFF,  _______, _______, KC_DEL,  \
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY_SE, QWERTY_US, NUMPAD, _______, _______, \
+  _______, RICKRL, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY_SE, QWERTY_US, NUMPAD,  PLOVER, _______, \
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______,   _______,   KC_NLCK, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______,   _______,   _______, _______, _______  \
 )
@@ -268,6 +293,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
             break;
+        case PLOVER:
+            if (record->event.pressed) {
+                #ifdef AUDIO_ENABLE
+                stop_all_notes();
+                PLAY_SONG(plover_song);
+                #endif
+                layer_off(_RAISE_SE);
+                layer_off(_LOWER_SE);
+                layer_off(_ADJUST);
+                layer_on(_PLOVER);
+                if (!eeconfig_is_enabled()) {
+                    eeconfig_init();
+                }
+                keymap_config.raw = eeconfig_read_keymap();
+                keymap_config.nkro = 1;
+                eeconfig_update_keymap(keymap_config.raw);
+            }
+            return false;
+            break;
+        case EXT_PLV:
+            if (record->event.pressed) {
+                #ifdef AUDIO_ENABLE
+                PLAY_SONG(plover_gb_song);
+                #endif
+                layer_off(_PLOVER);
+                layer_on(_QWERTY_US);
+            }
+            return false;
+            break;
         case BACKLIT:
             if (record->event.pressed) {
                 register_code(KC_RSFT);
@@ -290,11 +344,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING("->");
             } else {
             }
+            break;
         case KC_STD:
             if (record->event.pressed) {
                 SEND_STRING("std::");
             } else {
             }
+            break;
+        case RICKRL:
+        if (record->event.pressed) {
+            PLAY_SONG(rickroll);
+        }
+        return false;
+        break;
     }
     return true;
 }
