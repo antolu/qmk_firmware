1187c9cf6c50a30f6265e04cdd4e6f79e1e42e91
Anton Lu <anton96lu@gmail.com>
2020-11-15 16:51:28 +0100

    add mouse keys

--- keyboards/planck/keymaps/anton/keymap.c
+++ keyboards/planck/keymaps/anton/keymap.c
@@ -130,10 +130,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,      KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_H,   KC_J,     KC_K,    KC_L,    KC_SCOLON, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH,   KC_ENT,  \
-  KC_LCTL, KC_LGUI, KC_LGUI, KC_LALT, LOWER_US, KC_SPC, KC_SPC, RAISE_US, KC_LEFT, KC_DOWN, KC_UP,     KC_RGHT  \
+  KC_LCTL, KC_LALT, KC_LALT, KC_LGUI, LOWER_US, KC_SPC, KC_SPC, RAISE_US, KC_LEFT, KC_DOWN, KC_UP,     KC_RGHT  \
 ),
 
-/* Lower International  #@_&\$
+/* Lower International
  * ,-----------------------------------------------------------------------------------.
  * |   `  | Sup1 | Sup2 | Sup3 | Sup4 |      |      | Home | End  |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -155,17 +155,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   10 |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   *  |   +  |   -  |  =   |   /  |   %  |   (  |   )  |      | PgUp |  |   |
+ * |      |   *  |   +  |   -  |  =   |   /  |   %  |   (  |   )  |      | PgUp | PgUp |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      | PgDn |      |
+ * |      |      |      |      |      |      |      |      |      |      | PgDn | PgDn |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |PrtScr|      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE_US] = LAYOUT_planck_grid( \
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  KC_DEL,  KC_ASTR, KC_PLUS, KC_MINS, KC_EQL,  KC_SLSH, KC_PERC, KC_LPRN, KC_RPRN, _______, KC_PGUP, KC_PIPE, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, _______, \
+  KC_DEL,  KC_ASTR, KC_PLUS, KC_MINS, KC_EQL,  KC_SLSH, KC_PERC, KC_LPRN, KC_RPRN, KC_PIPE, _______, KC_PGUP, \
+  _______, KC_WH_D, KC_WH_U, KC_BTN2, KC_BTN1, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_PGDN, \
   _______, KC_PSCR, _______, _______, _______, KC_SPC,  KC_SPC,  _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
